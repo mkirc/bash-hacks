0ff3fd4623202b336eaf5fda1ab842ce91173e64
mkirc <m.p.kirchner@gmx.de>
2025-04-05 11:26:48 +0200

    adds comments to makeTensor, adds getTensorComponent

--- arrays/tensors.sh
+++ arrays/tensors.sh
@@ -4,64 +4,35 @@
 #
 # That said:
 
-getMatrixValue() {
-    local -n _matrix="$1"
-    shift
-    local -i max_level="$#"
-
-    local -a current_matrix="( $(catArray matrix) )"
-    local -i level=0  parameter_index=1 matrix_index
-    while [[ $level -lt $max_level ]]; do
-        # descend into matrix
-        matrix_index=${!parameter_index}
-        # echo "${current_matrix[$matrix_index]@Q}" | parseOneLevel
-        current_matrix=( "$(echo "${current_matrix[$matrix_index]@Q}" | parseOneLevel)" )
-        echo "${current_matrix[0]}"
-        declare -p current_matrix
-        # catArray current_matrix | breakLines
-        # return value
-        level+=1
-        parameter_index+=1
-    done
-    catArray current_matrix | breakLines
-}
-
-parseOneLevel() {
-    local -a tempArray="( ${*:-$(</dev/stdin)} )"
-    # catArray tempArray
-    echo "${tempArray[@]}" | while read line; do
-        echo "${line[@]}"
-    done
-}
-
-
-constructThreeByThree() {
-    local -a row=( 'a a' a a )
-    # construct 3x3 matrix
-    for i in {0..2}; do
-        printf '%s\n' "${row[*]@Q}"
-    done
-}
-
 makeTensor() {
-    local -i i dim="$#"
+    # Args: [Shape of Tensor (row-major order): Int...(Dim)]
+    # Returns: [Quoted Array of nested and escaped Arrays: String]
+    local -i dim max_dim="$#"
+
+    # Default value to initialize the tensor
     local default_val=0
-    for(( i=1; i<=$dim; i++ )); do
-        local current_dim=${!i}
-        local -i j
-        local -a current_row=()
-        if [[ $i -eq 1 ]]; then
-            for((j=0; j<$current_dim; j++)); do
+
+    # iterate over dimensions
+    for(( dim=1; dim<=$max_dim; dim++ )); do
+        local current_shape=${!dim}
+        local -i idx
+        # 1-d case: construct array. This will be used as a 'base' for higher
+        # dimensions or returned, if max_dim==1.
+        if [[ $dim -eq 1 ]]; then
+            local -a current_row=()
+            for((idx=0; idx<$current_shape; idx++)); do
                 current_row+=("$default_val")
             done
             local -a current_tensor="( $(echo ${current_row[@]@Q}) )"
         else
-            # declare -p current_tensor
+            # d>1-case: in order to properly escape and nest arrays, they will
+            # be interpreted as a single quoted String.
             local tensor_string="$(echo ${current_tensor[*]@Q})"
-            # local tensor_string=$(printf '%s\n' "${current_tensor[*]@Q}")
+
+            # in order to avoid duplication, current_tensor needs to be cleared
+            # and filled with properly encoded array representations
             current_tensor=()
-            for((j=0; j<$current_dim; j++)); do
-                # echo $tensor_string
+            for((idx=0; idx<$current_shape; idx++)); do
                 current_tensor+=( "$(echo $tensor_string)" )
             done
         fi
@@ -70,6 +41,13 @@ makeTensor() {
 }
 
 test_makeTensor() {
+    local -a current_tensor="( $(makeTensor 3 3) )"
+    declare -p current_tensor
+    local -a tensorMin1="( $(echo ${current_tensor[0]}) )"
+    declare -p tensorMin1
+    local -a tensorMin2="( $(echo ${tensorMin1[0]}) )"
+    declare -p tensorMin2
+
     local -a current_tensor="( $(makeTensor 2 2 2) )"
     declare -p current_tensor
     local -a tensorMin1="( $(echo ${current_tensor[0]}) )"
@@ -79,48 +57,60 @@ test_makeTensor() {
 
 }
 
-test_getMatrixValue() {
-    local -a matrix=( "$(constructThreeByThree)" )
-    declare -p matrix
-    local -a parsedMatrix="( $(catArray matrix | parseOneLevel | joinLines) )"
-    declare -p parsedMatrix
-    local -a row="( $(echo ${parsedMatrix[0]}) )"
-    declare -p row
-    local elm=$(echo ${row[0]})
-    declare -p elm
-    # local -a l1=( "$(catArray matrix | parseOneLevel)" )
-    # declare -p l1
-    # set -ex
-    # getMatrixValue matrix 0 0
-    # set +ex
-}
+getTensorComponent() {
+    # Args: [Index of desired component (row-major order): Int...(Dim)]
+    # Returns: Tensor of order-(Initial Dimension-Dim): String]
+    local -a current_tensor="( $(</dev/stdin) )"
+    local -i dim max_dim="$#"
 
-
-gatherAllArrays() {
-    # Args: [Name of Array to compare from: String] [Expression]
-    # Returns [Quoted string of array elements: String]
-    local -n array1="$1"
-    local -a out=()
-    local elm
-
-    shift
-
-    for elm in "${array1[@]}"; do
-        local -a result="( $(${@} ${elm}) )"
-        out+=("${result[@]@Q}")
-        echo "${result[@]@Q}"
+    # iteratively unpack nested arrays
+    for(( dim=1; dim<=$max_dim; dim++ )); do
+        local -i current_index=${!dim}
+        local -a current_tensor="( $(echo ${current_tensor[$current_index]}) )"
     done
+    echo "${current_tensor[@]@Q}"
 }
 
-test_gatherAllArrays() {
-    local -a testA=('a/a' 'a/b/b' 'c')
-    local -a outA="$(gatherAllArrays testA splitString '/')"
-    local -i i=0
-    catArray outA | breakLines | while read line; do
-        local -a arr="( ${line} )"
-        [[ $(catArray arr) == $( splitString '/' "${testA[$i]}") ]] && echo 'should work'
-        i+=1
-    done
+test_getTensorComponent() {
+    local -a twoByTwoByTwo="( $(makeTensor 2 2 2) )"
+    declare -p twoByTwoByTwo
+    local -a twoByTwo="( $(echo "${twoByTwoByTwo[@]@Q}" | getTensorComponent 0) )"
+    declare -p twoByTwo
+    local -a two="( $(echo "${twoByTwoByTwo[@]@Q}" | getTensorComponent 0 0) )"
+    declare -p two
+    local -a two2="( $(echo ${twoByTwo[@]@Q} | getTensorComponent 0) )"
+    declare -p two2
+    [[ $(echo "${two[@]@Q}") == $(echo "${two2[@]@Q}") ]] && echo 'should work'
+    local -a zero="( $(echo "${twoByTwoByTwo[@]@Q}" | getTensorComponent 0 0 0) )"
+    declare -p zero
 }
+
+
+# gatherAllArrays() {
+#     # Args: [Name of Array to compare from: String] [Expression]
+#     # Returns [Quoted string of array elements: String]
+#     local -n array1="$1"
+#     local -a out=()
+#     local elm
+
+#     shift
+
+#     for elm in "${array1[@]}"; do
+#         local -a result="( $(${@} ${elm}) )"
+#         out+=("${result[@]@Q}")
+#         echo "${result[@]@Q}"
+#     done
+# }
+
+# test_gatherAllArrays() {
+#     local -a testA=('a/a' 'a/b/b' 'c')
+#     local -a outA="$(gatherAllArrays testA splitString '/')"
+#     local -i i=0
+#     catArray outA | breakLines | while read line; do
+#         local -a arr="( ${line} )"
+#         [[ $(catArray arr) == $( splitString '/' "${testA[$i]}") ]] && echo 'should work'
+#         i+=1
+#     done
+# }
 
 source ./dispatch/recursive_dispatch.sh
